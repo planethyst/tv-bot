52aaacd5d859eb0986ad1553d6274da0a6cc5c85
Ash <ashleytheduck667@gmail.com>
2026-07-17 03:20:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
+import asyncio
 from os import getenv
 from discord.ext import commands
 import webbrowser
 from dotenv import load_dotenv
+import pyautogui 
 
 load_dotenv()
 TOKEN = getenv("TOKEN")
@@ -21,7 +23,12 @@ bot = commands.Bot(command_prefix=prefix, intents=intents)
 async def play(ctx, arg):
     if youtube in arg.lower():
         print(arg)
+        ctx.send("opening link for 5 minutes!")
         webbrowser.open(arg)
+        await asyncio.sleep(10) # so it actually plays the video LOL
+        pyautogui.press('space')
+        await asyncio.sleep(300)
+        os.system("taskkill /f /im firefox.exe") #make sure it closes after to prevent overloading the Evil Boy
     else:
         await ctx.send("this is not a youtube link!")
         print(arg)
