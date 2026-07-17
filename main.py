19bc646ebb8a9c03c65f3a19fcede24a65e2ffdf
Ash <ashleytheduck667@gmail.com>
2026-07-17 01:37:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import discord
+from discord.ext import commands
+import webbrowser
+
+
+
+
+youtube = "https://youtube.com/"
+prefix = "!"
+bot = commands.Bot(command_prefix=prefix)
+bot.run(token=lol)
+
+
+@bot.command()
+async def play(ctx, arg):
+    if youtube in arg:
+        await webbrowser.open(arg)
+    else:
+        await ctx.send("this is not a youtube link!")
