8cb362df5f78f366c6e0f0b32dfab5fd1dfd9afb
Ash <ashleytheduck667@gmail.com>
2026-07-17 02:24:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import discord
+from os import getenv
 from discord.ext import commands
 import webbrowser
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
 
 
+youtube = "https://youtube.com"
 
-
-youtube = "https://youtube.com/"
 prefix = "!"
-bot = commands.Bot(command_prefix=prefix)
-bot.run(token=lol)
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 
 
 @bot.command()
 async def play(ctx, arg):
-    if youtube in arg:
+    if youtube in str(arg).strip():
+        print(arg)
         await webbrowser.open(arg)
     else:
         await ctx.send("this is not a youtube link!")
+        print(arg)
+
+bot.run(token=TOKEN)
